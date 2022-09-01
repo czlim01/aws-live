073ca30879a6b20e98387040ab7c96332291760b
czlim01 <69498873+czlim01@users.noreply.github.com>
2022-09-01 14:50:18 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,6 @@
+customhost = "employee.cfmnjh6ybhob.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "limcheeziong-employee"
+customregion = "us-east-1"
